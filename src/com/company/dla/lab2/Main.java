b6866249fbb525a24347ddae4ebf76692b8911d0
Larisa914 <dlora914@gmail.com>
2021-04-20 16:30:42 +1000

    add lab2

---
+++ src/com/company/dla/lab2/Main.java
@@ -0,0 +1,9 @@
+package com.company.dla.lab2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello Lab2");
+    }
+}
