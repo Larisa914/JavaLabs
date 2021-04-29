19334f14df17a505a6723af242caee94d49fa0e5
Denis Tsyganov <dtsyganov@luxoft.com>
2021-04-29 20:42:40 +0600

    Lab 9 - условно Ok! Слишком сложный цикл while (break + 2 continue) refactoring + autoformat + renaming

--- src/com/company/dla/lab9/Main.java
+++ src/com/company/dla/lab9/Main.java
@@ -9,7 +9,7 @@ public class Main {
         // write your code here
         System.out.println("Hello Lab9!");
         TreeMap<String, TreeMap<String, Integer>> clients = new
-                TreeMap<String, TreeMap<String, Integer>>();
+                TreeMap<>();
 
         Scanner sc = new Scanner(System.in);
 // цикл ввода данных
@@ -19,7 +19,7 @@ public class Main {
                 break;
             }
             String[] parts = s.split(" ");
-            if(parts.length != 3){
+            if (parts.length != 3) {
                 System.out.println("Wrong number of arguments! Retry!");
                 continue;
             }
@@ -29,34 +29,32 @@ public class Main {
             Integer count;
             try {
                 count = Integer.parseInt(parts[2]);
-            } catch (NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 System.out.println(ex.getMessage());
                 System.out.println("Retry!");
                 continue;
             }
 
-            if (!clients.containsKey(name))
-                clients.put(name, new TreeMap <String, Integer>());
+            clients.putIfAbsent(name, new TreeMap<>());
             // структура карты покупателя
-            TreeMap <String, Integer> temp = clients.get(name);
+            TreeMap<String, Integer> temp = clients.get(name);
 
             // проверить наличие товара в карте покупателя,
             // если нет такого товара - добавляем с количеством 0
-            if (!temp.containsKey(productName))
-                temp.put(productName,0);
+            temp.putIfAbsent(productName, 0);
             // изменяем количество товара в карте покупателя
             Integer oldCount = temp.get(productName);
             temp.put(productName, oldCount + count);
         }
 // цикл для вывода карт пользователей
-        for(Map.Entry<String, TreeMap <String, Integer>> entry :
-            clients.entrySet()) {
+        for (Map.Entry<String, TreeMap<String, Integer>> entry :
+                clients.entrySet()) {
             String key = entry.getKey();
-            TreeMap <String, Integer> value = entry.getValue();
+            TreeMap<String, Integer> value = entry.getValue();
             //Выводим имя покупателя
             System.out.println(key + ":");
             //цикл для вывода продуктов
-            for(Map.Entry<String,Integer> product : value.entrySet()) {
+            for (Map.Entry<String, Integer> product : value.entrySet()) {
                 String keyProduct = product.getKey();
                 Integer valueProduct = product.getValue();
                 System.out.println(keyProduct + " " + valueProduct);
